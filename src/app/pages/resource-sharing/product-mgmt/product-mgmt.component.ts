d2eb06bae54e0337052ae8781f8f358694d41907
Mikiyas Zerfu <mickiaszerfu@gmail.com>
2023-11-17 08:42:11 +0300

    some fix on model and template

--- src/app/pages/resource-sharing/product-mgmt/product-mgmt.component.ts
+++ src/app/pages/resource-sharing/product-mgmt/product-mgmt.component.ts
@@ -1,34 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { ProductModel } from 'src/app/models/product.model';
+import { Resource } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-mgmt',
   templateUrl: './product-mgmt.component.html',
-  styleUrls: ['./product-mgmt.component.css' ]
+  styleUrls: ['./product-mgmt.component.css']
 })
 export class ProductMgmtComponent {
-  products: ProductModel[] = [
+  products: Resource[] = [
     // Add your product data here
     {
       id: 1,
-      name: 'Product 1',
+      title: 'Product 1',
       description: 'Description 1',
-      price: '10.99',
-      image_url:'https://dummyimage.com/300',
-      manufacturer: 'Manufacturer 1',
       category: {
         id: 1,
         name: 'Candles',
-        icon:'string'},
-      instock: 10,
-      isFeatured:true,
+        icon: 'string'
+      },
+      filename: 'string',
+      size: 2,
+      isFeatured: true,
       created_at: '2021-01-01',
       updated_at: '2021-01-01'
     },
-    // Add more products as needed
   ];
-  displayedColumns: string[] = ['id', 'name', 'image_url','description', 'price', 'manufacturer', 'category', 'instock', 'created_at', 'updated_at', 'action'];
+  displayedColumns: string[] = ['id', 'title', 'filename', 'description', 'size', 'category',  'created_at', 'updated_at', 'action'];
 
 
   constructor(private dialog: MatDialog) { }
